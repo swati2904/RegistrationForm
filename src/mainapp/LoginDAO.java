14ebfa2ca871c5b149e3a32eb57b3558074e7005
swati2904 <swati.lpu17@gmail.com>
2020-06-17 01:54:22 +0530

    added the validation for the user login

--- src/mainapp/LoginDAO.java
+++ src/mainapp/LoginDAO.java
@@ -1,9 +1,30 @@
 package mainapp;
 
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginDAO {
 
-	public boolean validate(LoginPojo loginpojo) {
-		// TODO Auto-generated method stub
+	public boolean validate(LoginPojo loginpojo) throws ClassNotFoundException, SQLException {
+		String  username = loginpojo.getUsername();
+		String  password = loginpojo.getPassword();
+
+		ConnectionManager con = new ConnectionManager();
+		Statement st = con.getConnection().createStatement();
+		
+		ResultSet rs =  st.executeQuery("SELECT * FROM REGISTRATION");
+		
+		while(rs.next()) {
+			if(username.equals(rs.getString("USERNAME")) && password.equals("PASSWORD")){
+				con.getConnection().close();
+				return true;
+			}
+			else {
+				con.getConnection().close();
+				return false;
+			}
+		}
 		return false;
 	}
 	
--- src/mainapp/Luck.java
+++ src/mainapp/Luck.java
@@ -3,8 +3,7 @@ package mainapp;
 public class Luck {
 
 	public void display() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Today will be a good day!!");
 	}
 
 }
